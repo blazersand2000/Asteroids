1ab1897259d2e87a207e73e5dfb9bb1c4fd7f35a
Andrew <blazersand2000@yahoo.com>
2025-10-14 00:07:51 -0400

    [WIP] Track velocity to use in explosion effect; delegate explosion effect to manager eventually

--- Groups.cs
+++ Groups.cs
@@ -7,4 +7,5 @@ public enum Groups
    Player,
    PlayerProjectile,
    Enemy,
+   Explodable
 }
--- Scenes/Components/ExplodableComponent.cs
+++ Scenes/Components/ExplodableComponent.cs
@@ -1,16 +1,22 @@
+using Asteroids;
 using Godot;
 using System;
 
 public partial class ExplodableComponent : Node2D
 {
+   [Signal]
+   public delegate void ExplosionRequestedEventHandler(Vector2 position, Vector2 velocity);
    [Export]
    public HurtboxComponent HurtboxComponent { get; set; }
+   [Export]
+   public MovementTrackerComponent MovementTrackerComponent { get; set; }
    [Export]
    public PackedScene ExplosionScene { get; set; }
 
    // Called when the node enters the scene tree for the first time.
    public override void _Ready()
    {
+      AddToGroup(Groups.Explodable.ToString());
       HurtboxComponent.Hit += OnHurtboxHit;
    }
 
@@ -21,6 +27,8 @@ public partial class ExplodableComponent : Node2D
 
    private void OnHurtboxHit(Vector2 hitPoint)
    {
+
+      EmitSignal(nameof(SignalName.ExplosionRequested), GlobalPosition, MovementTrackerComponent.GetVelocity());
       var parent = GetParent<Node2D>();
       // Not sure how best to do this, but for now adding child to grandparent since presumably parent will despawn since it's exploding
       var explosionOwner = parent.GetParent<Node2D>();
